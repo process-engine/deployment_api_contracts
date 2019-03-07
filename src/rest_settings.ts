ffb4392b256c07c89315cd2a3f398dd8a9a93137
ElRaptorus <christian.werner@5minds.de>
2019-03-07 10:33:38 +0100

    :book: Rewrite the code docs

--- src/rest_settings.ts
+++ src/rest_settings.ts
@@ -8,16 +8,7 @@ const paths = {
   undeployProcessModel: `/undeploy_process_model/${params.processModelId}`,
 };
 
-/**
- * Contains the endpoints and various rest parameters used by the deployment api.
- */
 export const restSettings = {
-  /**
-   * A collection of all url parameters employed by the deployment api.
-   */
   params: params,
-  /**
-   * A collection of all urls employed by the deployment api.
-   */
   paths: paths,
 };
